e0ee9f3ca743cd52caf20a40e9585d1567c0a634
mk-pmb <mk-pmb@users.noreply.github.com>
2023-07-03 05:13:42 +0200

    v0.1.1: divePathToString, allWithType

--- dive.mjs
+++ dive.mjs
@@ -2,13 +2,16 @@
 
 const hasOwn = Object.prototype.hasOwnProperty;
 
+function orf(x) { return (x || false); }
 function ensureArray(x) { return Array.isArray(x) ? x : [x]; }
 function applyFunc(f, args) { return f(...ensureArray(args)); }
 function divePathToString() { return this.join(' › '); }
 function diverToString() { return '[GraphDiver @ ' + this.basePath + ']'; }
 
-function makeGraphDiver(root, basePath) {
+const EX = function makeGraphDiver(root, basePath) {
   const bp = ensureArray(basePath || []);
+  bp.toString = divePathToString;
+
   function dive(prop, wantType) {
     if (prop === undefined) { return root; }
     if (Array.isArray(prop)) { return prop.reduce(applyFunc, dive); }
@@ -23,7 +26,7 @@ function makeGraphDiver(root, basePath) {
     }
     const c = root[prop];
     if (wantType !== undefined) {
-      const t = (c || false)['@type'];
+      const t = orf(c)['@type'];
       if (t !== wantType) {
         const err = new TypeError('Wrong entry @type: Expected ' + wantType
           + ' but got ' + t + ' @ ' + p);
@@ -34,12 +37,30 @@ function makeGraphDiver(root, basePath) {
         throw err;
       }
     }
-    return makeGraphDiver(c, p);
+    return EX(c, p);
   }
-  bp.toString = divePathToString;
-  dive.basePath = bp;
-  dive.toString = diverToString;
-  return dive;
-}
 
-export default makeGraphDiver;
+  Object.assign(dive, { basePath: bp }, EX.diverApi);
+  return dive;
+};
+
+
+EX.diverApi = {
+
+  toString: diverToString,
+
+  allWithType(wantType) {
+    const dive = this;
+    const root = dive();
+    const found = Object.values(root)
+      .filter(rec => ensureArray(orf(rec)['@type']).includes(wantType))
+      .map(rec => EX(rec));
+    return found;
+  },
+
+
+};
+
+
+
+export default EX;
